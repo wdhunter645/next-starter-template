f94246a1b089450884f2bef480d596320c3e9ad2
wdhunter645 <wdhunter13@aol.com>
2026-02-21 16:40:11 -0500

    fixing updated files for Task 13 Social Wall

--- src/components/SocialWall.tsx
+++ src/components/SocialWall.tsx
@@ -4,14 +4,15 @@ import styles from './social-wall.module.css';
 
 export default function SocialWall() {
   return (
-    <section id="social-wall" className="container section-gap">
-      <h2 className={styles.sectionTitle}>Social Wall</h2>
-
-      <div className={styles.embed}>
-        <div
-          className="elfsight-app-805f3c5c-67cd-4edf-bde6-2d5978e386a8"
-          data-elfsight-app-lazy
-        />
+    <section id="social-wall" className={styles.section}>
+      <div className={styles.container}>
+        <h2 className={styles.sectionTitle}>Social Wall</h2>
+        <div className={styles.embed}>
+          <div
+            className="elfsight-app-805f3c5c-67cd-4edf-bde6-2d5978e386a8"
+            data-elfsight-app-lazy
+          />
+        </div>
       </div>
     </section>
   );
