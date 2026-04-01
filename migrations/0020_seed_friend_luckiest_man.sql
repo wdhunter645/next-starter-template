6979c88d2a5cd98b7a98b0ddfda04a5222ef47bd
wdhunter645 <wdhunter13@aol.com>
2026-04-01 16:05:14 -0400

    Add Luckiest Man Lou Gehrig book to friends table

---
+++ migrations/0020_seed_friend_luckiest_man.sql
@@ -0,0 +1,10 @@
+-- Seed: Luckiest Man Lou Gehrig book
+INSERT INTO friends (name, kind, blurb, url, photo_url, status)
+VALUES (
+  'Luckiest Man: The Life and Death of Lou Gehrig',
+  'business',
+  'Definitive biography of Lou Gehrig by Jonathan Eig',
+  'https://www.jonathaneig.com/luckiest-man-the-life-and-death-of-lou-gehrig',
+  NULL,
+  'posted'
+);
